4a3489174063fcd11bd84e6b61a965ce6e8083fe
Scaled Cosine OOD <cosine.ood.detector@gmail.com>
2020-07-13 17:52:06 +0900

    Additional changed

--- train_supernet.py
+++ train_supernet.py
@@ -14,6 +14,7 @@ import torch.backends.cudnn as cudnn
 
 from utils import *
 from models.network import Network
+from tqdm import tqdm
 
 
 parser = argparse.ArgumentParser("cifar")
@@ -24,15 +25,15 @@ parser.add_argument('--learning_rate_min', type=float, default=0.0, help='min le
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
 parser.add_argument('--init_channels', type=int, default=36, help='num of init channels')
-parser.add_argument('--layers', type=int, default=10, help='total number of layers')
+parser.add_argument('--layers', type=int, default=8, help='total number of layers')
 parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
 parser.add_argument('--gpu', type=int, default=0, help='GPU device id')
-parser.add_argument('--epochs', type=int, default=50, help='num of training epochs')
+parser.add_argument('--epochs', type=int, default=200, help='num of training epochs')
 parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
 parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
 parser.add_argument('--save', type=str, default='./CheckPoints/', help='experiment path')
 parser.add_argument('--seed', type=int, default=9, help='random seed')
-parser.add_argument('--tmp_data_dir', type=str, default='/home/anhcda/Storage/ANAS/data/', help='temp data dir')
+parser.add_argument('--tmp_data_dir', type=str, default='/home/engkarat/data/storage/', help='temp data dir')
 parser.add_argument('--note', type=str, default='try', help='note for this run')
 parser.add_argument('--cifar100', action='store_true', default=False, help='search with cifar100 dataset')
 
@@ -63,7 +64,7 @@ def main():
     np.random.seed(args.seed)
     cudnn.benchmark = True
     torch.manual_seed(args.seed)
-    cudnn.enabled=True
+    cudnn.enabled = True
     torch.cuda.manual_seed(args.seed)
     logging.info("args = %s", args)
     logging.info("unparsed args = %s", unparsed)
@@ -74,11 +75,11 @@ def main():
     else:
         train_transform, valid_transform = utils._data_transforms_cifar10(args)
     if args.cifar100:
-        train_data = dset.CIFAR100(root=args.tmp_data_dir, train=True, download=False, transform=train_transform)
-        valid_data = dset.CIFAR100(root=args.tmp_data_dir, train=False, download=False, transform=valid_transform)
+        train_data = dset.CIFAR100(root=args.tmp_data_dir, train=True, download=True, transform=train_transform)
+        valid_data = dset.CIFAR100(root=args.tmp_data_dir, train=False, download=True, transform=valid_transform)
     else:
-        train_data = dset.CIFAR10(root=args.tmp_data_dir, train=True, download=False, transform=train_transform)
-        valid_data = dset.CIFAR10(root=args.tmp_data_dir, train=False, download=False, transform=valid_transform)
+        train_data = dset.CIFAR10(root=args.tmp_data_dir, train=True, download=True, transform=train_transform)
+        valid_data = dset.CIFAR10(root=args.tmp_data_dir, train=False, download=True, transform=valid_transform)
 
     train_queue = torch.utils.data.DataLoader(
         train_data, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=args.workers)
@@ -91,6 +92,10 @@ def main():
     criterion = criterion.cuda()
     supernet = Network(args.init_channels, CIFAR_CLASSES, args.layers)
     supernet.cuda()
+    print(len(supernet.cells))
+    # exit()
+    # ckpt = torch.load('./CheckPoints/eval-try-20200710-221619/weights.pt')
+    # supernet.load_state_dict(ckpt)
 
     optimizer = torch.optim.SGD(
         supernet.parameters(),
@@ -106,10 +111,10 @@ def main():
         train_acc, train_obj = train(train_queue, supernet, criterion, optimizer)
         logging.info('train_acc %f', train_acc)
 
-        ops_alps, att_alps = utils.get_child_alphas(supernet._layers, supernet._steps, free=False)
-        subnet = supernet.get_sub_net(ops_alps, att_alps)
+        # ops_alps, att_alps = utils.get_child_alphas(supernet._layers, supernet._steps, free=False)
+        # subnet = supernet.get_sub_net(ops_alps, att_alps)
 
-        valid_acc, valid_obj = infer(valid_queue, subnet, criterion)
+        valid_acc, valid_obj = infer(valid_queue, supernet, criterion)
         logging.info('valid_acc %f', valid_acc)
         scheduler.step()
 
@@ -126,7 +131,7 @@ def train(train_queue, model, criterion, optimizer):
         target = target.cuda(non_blocking=True)
 
         model.zero_grad()
-        model.generate_cell_alphas()
+        model.generate_cell_alphas() # change the architecture
 
         logits = model(input)
         loss = criterion(logits, target)
@@ -137,6 +142,7 @@ def train(train_queue, model, criterion, optimizer):
         n = input.size(0)
         objs.update(loss.clone().item(), n)
         top1.update(prec1.clone().item(), n)
+        step += 1
 
         if step % args.report_freq == 0:
             logging.info('Train Step: %03d Objs: %e Acc: %f', step, objs.avg, top1.avg)
@@ -148,6 +154,7 @@ def infer(valid_queue, model, criterion):
     objs = utils.AverageMeter()
     top1 = utils.AverageMeter()
     model.eval()
+    model.generate_cell_alphas(is_infer=True)
 
     for step, (input, target) in enumerate(valid_queue):
         input = input.cuda(non_blocking=True)
@@ -161,6 +168,7 @@ def infer(valid_queue, model, criterion):
         n = input.size(0)
         objs.update(loss.clone().item(), n)
         top1.update(prec1.clone().item(), n)
+        step += 1
 
         if step % args.report_freq == 0:
             logging.info('Valid Step: %03d Objs: %e Acc: %f', step, objs.avg, top1.avg)
